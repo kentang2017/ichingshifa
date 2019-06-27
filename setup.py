cc5b3d16a52cf0da981cb80e6f930da103b34db1
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2019-06-27 10:12:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
-
+   
 setuptools.setup(
     name="iching_shifa",
     version="1.0",
