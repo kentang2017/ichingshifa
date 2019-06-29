8b7882f0a781ae18e983ad2dc5a1f0106461e793
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2019-06-29 12:31:37 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
    
 setuptools.setup(
-    name="iching_shifa",
-    version="1.0",
+    name="ichingshifa",
+    version="2.7.3",
     author="Ken Tang",
     author_email="kinyeah@gmail.com",
-    description="A package of iching stalk divination (texts written in Traditional Chinese)",
+    description="A package of iching stalk divination (texts written in Traditional Chinese). Python周易筮法，起卦不求人。",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kentang2017/iching_shifa",
