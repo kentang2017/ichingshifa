2fe98f92cb48aa8b81df2dec5be1224b65a3e391
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2019-07-12 11:09:52 +0800

    Update ichingshifa.py

--- ichingshifa/ichingshifa.py
+++ ichingshifa/ichingshifa.py
@@ -18,6 +18,48 @@ def multi_key_dict_get(d, k):
             return v
     return None
 
+def mget_bookgua_details(guayao):
+    getgua = multi_key_dict_get(sixtyfourgua, guayao)
+    yao_results = sixtyfourgua_description.get(getgua)
+    bian_yao = guayao.replace("6","1").replace("9","1").replace("7","0").replace("8","0")
+    dong_yao = bian_yao.count("1")
+    explaination = "動爻有【"+str(dong_yao )+"】根。"
+    dong_yao_change = guayao.replace("6","7").replace("9","8")
+    g_gua = multi_key_dict_get(sixtyfourgua, dong_yao_change)
+    g_gua_result = sixtyfourgua_description.get(g_gua)
+    b_gua_n_g_gua = "【"+getgua+"之"+g_gua+"】"
+    top_bian_yao = bian_yao.rfind("1")+int(1)
+    second_bian_yao = bian_yao.rfind("1",0, bian_yao.rfind("1"))+int(1)
+    top_jing_yao = bian_yao.rfind("0") + int(1)
+    second_jing_yao = bian_yao.rfind("0", 0, bian_yao.rfind("0"))+int(1)
+    top = yao_results.get(top_bian_yao)
+    second = yao_results.get(second_bian_yao)
+    top_2 = yao_results.get(top_jing_yao)
+    second_2 = yao_results.get(second_jing_yao)
+    explaination2 = None
+    try:
+        if dong_yao == 0:
+            explaination2 = explaination, "主要看【"+getgua+"】卦彖辭。",  yao_results[7][2:]
+        elif dong_yao == 1: 
+            explaination2 = explaination, b_gua_n_g_gua, "主要看【"+top[:2]+"】",  top
+        elif dong_yao == 2:
+            explaination2 = b_gua_n_g_gua, explaination, "主要看【"+top[:2]+"】，其次看【"+second[:2]+"】。", top, second
+        elif dong_yao == 3:
+            if bian_yao.find("1") == 0:
+                explaination2 = b_gua_n_g_gua, explaination,  "【"+getgua+"】卦為貞(我方)，【"+g_gua+"】卦為悔(他方)。前十卦，主貞【"+getgua+"】卦，請參考兩卦彖辭", yao_results[7][2:], g_gua_result[7][2:]
+            elif bian_yao.find("1") > 0:
+                explaination2 = b_gua_n_g_gua, explaination,  "【"+getgua+"】卦為貞(我方)，【"+g_gua+"】卦為悔(他方)。後十卦，主悔【"+g_gua+"】卦，請參考兩卦彖辭", g_gua_result[7][2:],  yao_results[7][2:]
+        elif dong_yao == 4:
+            explaination2 = b_gua_n_g_gua, explaination, "主要看【"+g_gua+"】的"+g_gua_result.get(second_jing_yao)[:2]+"，其次看"+g_gua_result.get(top_jing_yao)[:2]+"。", g_gua_result.get(second_jing_yao), g_gua_result.get(top_jing_yao)
+        elif dong_yao == 5:    
+            explaination2 = b_gua_n_g_gua, explaination,  "主要看【"+g_gua+"】的"+g_gua_result.get(top_jing_yao)[:2]+"。", g_gua_result.get(top_jing_yao)
+        elif dong_yao == 6:
+            explaination2 = b_gua_n_g_gua, explaination, "主要看【"+g_gua+"】卦的彖辭。", g_gua_result[7][2:]
+    except (TypeError, UnboundLocalError):
+        pass
+    return [guayao, getgua, g_gua, yao_results, explaination2]
+
+
 def bookgua(): #由底至上起爻
     shifa_results = []
     for i in range(6):
@@ -59,48 +101,6 @@ def bookgua(): #由底至上起爻
         shifa_results.append(yao)
     return "".join(str(e) for e in shifa_results[:6])
 
-def bookgua_details():
-    guayao = bookgua()
-    getgua = multi_key_dict_get(sixtyfourgua, guayao)
-    yao_results = sixtyfourgua_description.get(getgua)
-    bian_yao = guayao.replace("6","1").replace("9","1").replace("7","0").replace("8","0")
-    dong_yao = bian_yao.count("1")
-    explaination = "動爻有【"+str(dong_yao )+"】根。"
-    dong_yao_change = guayao.replace("6","7").replace("9","8")
-    g_gua = multi_key_dict_get(sixtyfourgua, dong_yao_change)
-    g_gua_result = sixtyfourgua_description.get(g_gua)
-    b_gua_n_g_gua = "【"+getgua+"之"+g_gua+"】"
-    top_bian_yao = bian_yao.rfind("1")+int(1)
-    second_bian_yao = bian_yao.rfind("1",0, bian_yao.rfind("1"))+int(1)
-    top_jing_yao = bian_yao.rfind("0") + int(1)
-    second_jing_yao = bian_yao.rfind("0", 0, bian_yao.rfind("0"))+int(1)
-    top = yao_results.get(top_bian_yao)
-    second = yao_results.get(second_bian_yao)
-    top_2 = yao_results.get(top_jing_yao)
-    second_2 = yao_results.get(second_jing_yao)
-    explaination2 = None
-    try:
-        if dong_yao == 0:
-            explaination2 = "主要看【"+getgua+"】卦彖辭。",  yao_results[7][2:]
-        elif dong_yao == 1: 
-            explaination2 = b_gua_n_g_gua, "主要看【"+top[:2]+"】",  top
-        elif dong_yao == 2:
-            explaination2 = b_gua_n_g_gua, "主要看【"+top[:2]+"】，其次看【"+second[:2]+"】。", top, second
-        elif dong_yao == 3:
-            if bian_yao.find("1") == 0:
-                explaination2 = b_gua_n_g_gua, "【"+getgua+"】卦為貞(我方)，【"+g_gua+"】卦為悔(他方)。前十卦，主貞【"+getgua+"】卦，請參考兩卦彖辭", yao_results[7][2:], g_gua_result[7][2:]
-            elif bian_yao.find("1") > 0:
-                explaination2 = b_gua_n_g_gua, "【"+getgua+"】卦為貞(我方)，【"+g_gua+"】卦為悔(他方)。後十卦，主悔【"+g_gua+"】卦，請參考兩卦彖辭", g_gua_result[7][2:],  yao_results[7][2:]
-        elif dong_yao == 4:
-            explaination2 = b_gua_n_g_gua, "主要看【"+g_gua+"】的"+g_gua_result.get(second_jing_yao)[:2]+"，其次看"+g_gua_result.get(top_jing_yao)[:2]+"。", g_gua_result.get(second_jing_yao), g_gua_result.get(top_jing_yao)
-        elif dong_yao == 5:    
-            explaination2 = b_gua_n_g_gua, "主要看【"+g_gua+"】的"+g_gua_result.get(top_jing_yao)[:2]+"。", g_gua_result.get(top_jing_yao)
-        elif dong_yao == 6:
-            explaination2 = b_gua_n_g_gua, "主要看【"+g_gua+"】卦的彖辭。", g_gua_result[7][2:]
-    except (TypeError, UnboundLocalError):
-        pass
-    return [guayao, getgua,  yao_results, g_gua, explaination, explaination2]
-
 def datetime_bookgua(year, month, day, hour):
     upper_gua_remain = (year+month+day) % 8
     if upper_gua_remain is 0:
@@ -120,6 +120,9 @@ def datetime_bookgua(year, month, day, hour):
     description = multi_key_dict_get(sixtyfourgua_description,  ben_gua)
     g_gua = multi_key_dict_get(sixtyfourgua, (bian_gua.replace("6", "7").replace("9", "8")))
     return ben_gua+"之"+g_gua, eightgua_element.get(upper_gua_remain)+eightgua_element.get(lower_gua_remain)+ben_gua , "變爻為"+description[bian_yao][:2], description[bian_yao][3:]
+    
+def bookgua_details():
+    return mget_bookgua_details(bookgua())
     
 def current_bookgua():
     now = datetime.datetime.now()
