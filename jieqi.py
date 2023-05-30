d4c442461164db40926e9d9f505bed8c8e0f6d17
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-05-30 11:24:54 +0800

    Update jieqi.py

--- jieqi.py
+++ jieqi.py
@@ -8,9 +8,19 @@ Created on Tue May  9 20:32:01 2023
 import re
 from math import pi
 from ephem import Sun, Date, Ecliptic, Equatorial
-import config
 
 jieqi_name = re.findall('..', '春分清明穀雨立夏小滿芒種夏至小暑大暑立秋處暑白露秋分寒露霜降立冬小雪大雪冬至小寒大寒立春雨水驚蟄')
+
+def multi_key_dict_get(d, k):
+    for keys, v in d.items():
+        if k in keys:
+            return v
+    return None
+
+def new_list(olist, o):
+    a = olist.index(o)
+    res1 = olist[a:] + olist[:a]
+    return res1
 
 def ecliptic_lon(jd_utc):
     return Ecliptic(Equatorial(Sun(jd_utc).ra,Sun(jd_utc).dec,epoch=jd_utc)).lon
@@ -58,7 +68,7 @@ def gong_wangzhuai():
                         ('立冬','小雪','大雪'):'立冬',
                         ('冬至','小寒','大寒'):'冬至',
                         ('立春','雨水','驚蟄'):'立春'}
-    return dict(zip(config.new_list(wangzhuai_num, dict(zip(jieqi_name[0::3],wangzhuai_num )).get(config.multi_key_dict_get(wangzhuai_jieqi, "霜降"))), wangzhuai))
+    return dict(zip(new_list(wangzhuai_num, dict(zip(jieqi_name[0::3],wangzhuai_num )).get(multi_key_dict_get(wangzhuai_jieqi, "霜降"))), wangzhuai))
 
 def xzdistance(year, month, day, hour):
     return int(find_jq_date(year, month, day, hour, "夏至") -  Date("{}/{}/{} {}:00:00.00".format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2), str(hour).zfill(2))))
