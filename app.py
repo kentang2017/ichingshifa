295b186fec6dfb8bb59cfb8edd7cf0bd2e862b9e
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-10-10 23:40:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,19 +40,26 @@ with st.sidebar:
     d = int(p[2])
     h = int(pp[0])
     min = int(pp[1])
+    st.write("手動起爻︰(初爻由下而上)")
+    option_sixth = st.selectbox(
+         '上爻',
+        ('老陰', '少陰', '少陽', '老陽'))
     option_first = st.selectbox(
-        '手動起爻︰(初爻由下而上)',
+        '五爻',
         ('老陰', '少陰', '少陽', '老陽'))
     option_second = st.selectbox(
+        '四爻',
         ('老陰', '少陰', '少陽', '老陽'))
     option_third = st.selectbox(
+         '三爻',
         ('老陰', '少陰', '少陽', '老陽'))
     option_forth = st.selectbox(
+         '二爻',
         ('老陰', '少陰', '少陽', '老陽'))
     option_fifth = st.selectbox(
+         '初爻',
         ('老陰', '少陰', '少陽', '老陽'))
-    option_sixth = st.selectbox(
-        ('老陰', '少陰', '少陽', '老陽'))
+    
     yaodict = {"老陰": 6, '少陽':7, "老陽": 9, '少陰':8 }
     st.write('You selected:',  "".join([yaodict.get(i) for i in [option_sixth, option_fifth, option_forth, option_third, option_second, option_first]]))
 
