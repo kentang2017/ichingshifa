ef3728da02c686005671d44b2d6bc94f8c374072
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-10-10 23:11:32 +0800

    Update jieqi.py

--- ichingshifa/jieqi.py
+++ ichingshifa/jieqi.py
@@ -12,6 +12,17 @@ from ephem import Sun, Date, Ecliptic, Equatorial
 
 jieqi_name = re.findall('..', '春分清明穀雨立夏小滿芒種夏至小暑大暑立秋處暑白露秋分寒露霜降立冬小雪大雪冬至小寒大寒立春雨水驚蟄')
 
+def multi_key_dict_get(d, k):
+    for keys, v in d.items():
+        if k in keys:
+            return v
+    return None
+
+def new_list(olist, o):
+    a = olist.index(o)
+    res1 = olist[a:] + olist[:a]
+    return res1
+
 def ecliptic_lon(jd_utc):
     s=Sun(jd_utc)
     equ=Equatorial(s.ra,s.dec,epoch=jd_utc)
@@ -79,6 +90,6 @@ def gong_wangzhuai():
                         ('立冬','小雪','大雪'):'立冬',
                         ('冬至','小寒','大寒'):'冬至',
                         ('立春','雨水','驚蟄'):'立春'}
-    return dict(zip(config.new_list(wangzhuai_num, dict(zip(jieqi_name[0::3],wangzhuai_num )).get(config.multi_key_dict_get(wangzhuai_jieqi, "霜降"))), wangzhuai))
+    return dict(zip(new_list(wangzhuai_num, dict(zip(jieqi_name[0::3],wangzhuai_num )).get(multi_key_dict_get(wangzhuai_jieqi, "霜降"))), wangzhuai))
 
 
