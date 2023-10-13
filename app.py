4d2f4d3e54cd722170b2d2b0bb8bf7e864298c37
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-10-13 15:59:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,9 +83,12 @@ with pan:
     pan2 = ichingshifa.Iching().display_pan_m(y,m,d,h,min,combine)
     output2 = st.empty()
     with st_capture(output2.code):
-        if manual:
-            print(pan2)
-        else:
+        try:
+            if manual:
+                print(pan2)
+            else:
+                print(pan)
+        except (ValueError, UnboundLocalError):
             print(pan)
 
 
