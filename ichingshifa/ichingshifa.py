b765cfbc00248869337631b37c08b6c29d365962
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-08-29 17:16:18 +0800

    Update ichingshifa.py

--- ichingshifa/ichingshifa.py
+++ ichingshifa/ichingshifa.py
@@ -556,7 +556,7 @@ class Iching():
 
 #後天策軌數
     def acquired_cegui(self, year, month, day, hour, minute):
-        acquired_gua = list("ｏ坎坤震巽坤乾兌艮離")
+        acquired_gua = list("ｏ坎坤震巽ｏ乾兌艮離")
         innate_kinkun_num = {tuple([7,9]):768,tuple([6,8]):672}
         dongyao_position = {tuple([0,1,2]):"下卦",tuple([3,4,5]):"上卦"}
         gua = self.qigua_time(year, month, day, hour, minute).get("大衍筮法")[0]
@@ -580,10 +580,11 @@ class Iching():
         if innate_num < 10000:
             innate_num = str(innate_num).zfill(5)
         num_to_wuxing = dict(zip(list(range(0,10)),list("空水火木金土水火木金土")))
+        ac_num = [cn2an.transform(str(i), "an2cn") for i in list(str(innate_num))]
         return  [list("萬元會運世"),  
                  [cn2an.transform(str(i), "an2cn") for i in list(str(innate_num))], 
                  [dict(zip(range(0,10), acquired_gua)).get(int(i)) for i in list(str(innate_num))],
-                 [num_to_wuxing.get(int(i)) for i in list(str(innate_num))]]
+                 [num_to_wuxing.get(int(i)) for i in list(str(innate_num))]], acquire_num_dict.get("".join(ac_num))
 	
     def display_pan_m(self, year, month, day, hour, minute, mgua):
         gz = self.gangzhi(year, month, day, hour, minute)
