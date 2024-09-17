0173327cc75791f55cf72b34b06276383c217fde
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-09-17 16:09:19 +0800

    Update ichingshifa.py

--- ichingshifa/ichingshifa.py
+++ ichingshifa/ichingshifa.py
@@ -490,11 +490,12 @@ class Iching():
         zhi_code = dict(zip(self.dizhi, range(1,13)))
         yz_code = zhi_code.get(gangzhi[0][1])
         hz_code = zhi_code.get(gangzhi[3][1])
+	min_code = zhi_code.get(gangzhi[4][1])
         cm = ld.get("月")
         cd =  ld.get("日")
         eightgua = self.data.get("八卦數值")
-        lower_gua_remain = (yz_code +cm+cd+hz_code) % 8
-        upper_gua_remain = (yz_code+cm+cd) % 8
+        lower_gua_remain = (yz_code +cm+cd+hz_code+min_code) % 8
+        upper_gua_remain = (yz_code+cm+cd+hz_code) % 8
         if upper_gua_remain == 0:
             upper_gua_remain = int(8)
         upper_gua = eightgua.get(upper_gua_remain)
